236384d80860ad47d62b5cde3be33b2c56096b57
Marcus Tenedini <166030842+marcustenedini@users.noreply.github.com>
2024-07-30 16:18:58 -0300

    Update Monad.scala

--- src/main/scala/br/unb/cic/flang/Monad.scala
+++ src/main/scala/br/unb/cic/flang/Monad.scala
@@ -1,25 +1,19 @@
 package br.unb.cic.flang
+import cats.data.State
+import cats.implicits._
 
 package object StateMonad {
   type S = List[(String, Integer)]
+  type M[A]=State[S,A]
 
-  case class M[A](f: S => (A, S))
+  def pure[A](a: A): M[A] = State.pure(a)
 
-  def pure[A](a: A): M[A] = M[A] { s => (a, s) }
+  def bind[A, B](m: M[A])(f: A => M[B]): M[B] = m.flatMap(f)
 
-  def bind[A, B](m: M[A])(f: A => M[B]): M[B] = M({ s: S =>
-    {
-      val (a, s1) = runState(m)(s)
-      val (b, s2) = runState(f(a))(s1)
-      (b, s2)
-    }
-  })
+  def runState[A](s: M[A]): (S => (A, S)) = s.run(_).value.swap
 
-  def runState[A](s: M[A]): (S => (A, S)) = s.f
-
-  def put(s: S): M[Unit] = M({ _: S => ((), s) })
-
-  def get[A](): M[S] = M({ s: S => (s, s) })
+  def put(s: S): M[Unit] = State.set(s)
+  def get[A](): M[S] = State.get
 
   def declareVar(name: String, value: Integer, state: S): S =
     (name, value) :: state
